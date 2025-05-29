5956c349aa661081fdb3a2b2d3c6027bfbd78563
ludwigenvall <ludwigenv@gmail.com>
2025-05-29 16:41:58 +0200

    Update lstm_model.py
    Fixed error with combining sequences to tensor

--- scripts/lstm_model.py
+++ scripts/lstm_model.py
@@ -9,21 +9,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 
-# Function to combine the 3 sequence columns into a 3D array for the LSTM model
-# (n_samples, 30 timesteps, 3 features)
+# Function to combine the 3 sequence columns into a tensor for the LSTM model
 def prepare_lstm_data(df):
-    sequences = np.stack([
-        df['logins_seq'].to_list(),
-        df['support_seq'].to_list(),
-        df['data_seq'].to_list()
-    ], axis=-1)
-    
+    # n_samples = number of customers, 30 timesteps, 3 features
+    n_samples = len(df)
+    n_timesteps = len(df.iloc[0]['logins_seq'])
+
+    # Preallocate array
+    sequences = np.zeros((n_samples, n_timesteps, 3))
+
+    # Fill features
+    sequences[:, :, 0] = np.array(df['logins_seq'].to_list())
+    sequences[:, :, 1] = np.array(df['support_seq'].to_list())
+    sequences[:, :, 2] = np.array(df['data_seq'].to_list())
+
     # Encodes "Churn" as 0=No, 1=Yes
     le = LabelEncoder()
     labels = le.fit_transform(df['Churn'])
 
     # Returns a split into training/test datasets
     return train_test_split(sequences, labels, test_size=0.2, random_state=42)
+
+
+
 
 # Function to build baseline model
 def build_lstm_model(input_shape):
