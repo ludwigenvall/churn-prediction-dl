2d99b56d5e510b5a689369a49266c61e850e770e
ludwigenvall <ludwigenv@gmail.com>
2025-05-29 16:59:10 +0200

    Update lstm_model.py

--- scripts/lstm_model.py
+++ scripts/lstm_model.py
@@ -11,32 +11,17 @@ from sklearn.preprocessing import LabelEncoder
 
 # Function to combine the 3 sequence columns into a tensor for the LSTM model
 def prepare_lstm_data(df):
-    #Only filter rows where all sequences have length 30
-    valid_idx = df.apply(lambda row: 
-                         len(row['logins_seq']) == 30 and 
-                         len(row['support_seq']) == 30 and 
-                         len(row['data_seq']) == 30, axis=1)
-    
-    df = df[valid_idx].reset_index(drop=True)
-    
-    # n_samples = number of customers, 30 timesteps, 3 features
-    n_samples = len(df)
-    n_timesteps = 30
-
-    # Preallocate array
-    sequences = np.zeros((n_samples, n_timesteps, 3))
-
-    # Fill features
-    sequences[:, :, 0] = np.array(df['logins_seq'].to_list())
-    sequences[:, :, 1] = np.array(df['support_seq'].to_list())
-    sequences[:, :, 2] = np.array(df['data_seq'].to_list())
-
+    sequences = np.stack([
+        df['logins_seq'].to_list(),
+        df['support_seq'].to_list(),
+        df['data_seq'].to_list()
+    ], axis=1).transpose(1, 2, 0)  # Dimension fix
     # Encodes "Churn" as 0=No, 1=Yes
     le = LabelEncoder()
     labels = le.fit_transform(df['Churn'])
 
     # Returns a split into training/test datasets
-    return train_test_split(sequences, labels, test_size=0.2, random_state=42)
+    return train_test_split(sequences, labels, test_size=0.3, random_state=42)
 
 
 
